3bf91ae6a02cea9ba2b531e92b3f1b188ff04241
Martin Odersky <odersky@gmail.com>
2018-10-17 18:00:55 +0200

    Fix tests

--- compiler/src/dotty/tools/dotc/parsing/Parsers.scala
+++ compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -2480,6 +2480,7 @@ object Parsers {
       newLineOptWhenFollowedBy(LBRACE)
       if ((name.isEmpty || parents.isEmpty) && in.token != LBRACE)
         syntaxErrorOrIncomplete(ExpectedTokenButFound(LBRACE, in.token))
+      var mods1 = addMod(mods, witnessMod)
       val wdef =
         if (in.token == LBRACE) {
           val templ = templateBodyOpt(makeConstructor(tparams, vparamss), parents, isEnum = false)
@@ -2493,9 +2494,14 @@ object Parsers {
               expr()
             }
             else EmptyTree
-          DefDef(name, tparams, vparamss, constrAppsToType(parents), rhs)
+          val tpt = constrAppsToType(parents)
+          if (tparams.isEmpty && vparamss.isEmpty) {
+            mods1 |= Lazy
+            ValDef(name, tpt, rhs)
+          }
+          else DefDef(name, tparams, vparamss, tpt, rhs)
         }
-      finalizeDef(wdef, addMod(mods, witnessMod), start)
+      finalizeDef(wdef, mods1, start)
     }
 
 /* -------- TEMPLATES ------------------------------------------- */
